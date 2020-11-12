bd1ea3326182b164f22390f12ff52e207385c539
liuyu <154650362@qq.com>
2020-11-12 22:45:54 +0800

    L7

--- model/User.go
+++ model/User.go
@@ -29,7 +29,7 @@ func CheckUser(name string) (code int) {
 
 // 新增用户
 func CreateUser(data *User) int {
-	data.Password = ScryprPw(data.Password) //加密密码
+	//data.Password = ScryprPw(data.Password) //加密密码
 	err := db.Create(&data).Error
 	if err != nil {
 		return errmsg.ERROR //500
@@ -48,10 +48,33 @@ func GetUsers(pageSize int, pageNum int) []User {
 }
 
 // 编辑用户
+func EditUser(id int, data *User) int {
+	var user User
+	var maps = make(map[string]interface{})
+	maps["username"] = data.Username
+	maps["role"] = data.Role
+	err = db.Model(&user).Where("id = ? ", id).Update(maps).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+}
 
 // 删除用户
+func DeleteUser(id int) int {
+	var user User
+	err = db.Where("id = ? ", id).Delete(&user).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+}
 
 // 密码加密
+func (u *User) BeforeSave() { //框架实现了钩子
+	u.Password = ScryprPw(u.Password)
+}
+
 func ScryprPw(password string) string {
 	const KeyLen = 10
 	salt := make([]byte, 8)
