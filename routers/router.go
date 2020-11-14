18d0342ee1173a894d9eb6ce78b188540011f003
liuyu <154650362@qq.com>
2020-11-14 22:59:11 +0800

    jwt

--- routers/router.go
+++ routers/router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	v1 "ginblog/api/v1"
+	"ginblog/middleware"
 	"ginblog/utils"
 	"github.com/gin-gonic/gin"
 )
@@ -10,26 +11,32 @@ func InitRouter() {
 	gin.SetMode(utils.AppMode)
 	r := gin.Default()
 
-	router := r.Group("api/v1")
+	auth := r.Group("api/v1")
+	auth.Use(middleware.JwtToken())
 	{
 		//User模块的路由接口
+		//auth.POST("user/add", v1.AddUser)
+
+		auth.PUT("user/:id", v1.EditUser)
+		auth.DELETE("user/:id", v1.DeleteUser)
+		//分类模块的路由借口
+		auth.POST("category/add", v1.AddCategory)
+		auth.PUT("category/:id", v1.EditCate)
+		auth.DELETE("category/:id", v1.DeleteCate)
+		//文章模块的路由接口
+		auth.POST("article/add", v1.AddArt)
+		auth.PUT("article/:id", v1.EditArt)
+		auth.DELETE("article/:id", v1.DeleteArt)
+	}
+	router := r.Group("api/v1")
+	{
 		router.POST("user/add", v1.AddUser)
 		router.GET("users", v1.GetUsers)
-		router.PUT("user/:id", v1.EditUser)
-		router.DELETE("user/:id", v1.DeleteUser)
-		//分类模块的路由借口
-		router.POST("category/add", v1.AddCategory)
 		router.GET("category", v1.GetCate)
-		router.PUT("category/:id", v1.EditCate)
-		router.DELETE("category/:id", v1.DeleteCate)
-		//文章模块的路由接口
-		router.POST("article/add", v1.AddArt)
 		router.GET("article", v1.GetArt)
 		router.GET("article/list/:id", v1.GetCateArt)
 		router.GET("article/info/:id", v1.GetArtInfo)
-		router.PUT("article/:id", v1.EditArt)
-		router.DELETE("article/:id", v1.DeleteArt)
-
+		router.POST("login", v1.Login)
 	}
 
 	r.Run(utils.HttpPort)
